755bf37ff530c044f13e1bd5181bc4fe9b1dd8ae
Nimish <nmd2611@gmail.com>
2020-03-05 12:56:18 +0530

    assign 7 completed

--- assign7.cpp
+++ assign7.cpp
@@ -16,11 +16,11 @@ public:
 	string type;
 	//string size;
 	int chain;
-	Node *link;
+
 	Node()
 	{
 		key=type="";
-		link=NULL;
+
 		chain=-1;
 
 	}
@@ -107,7 +107,7 @@ public:
 		{
 			table[pos].key=k;
 			table[pos].type=t;
-			table[pos].link=NULL;
+
 		}
 		else
 		{
@@ -147,7 +147,7 @@ public:
 		{
 			table[pos].key=k;
 			table[pos].type=t;
-			table[pos].link=NULL;
+
 		}
 		else
 		{
@@ -175,34 +175,133 @@ public:
 				if(table[pos].key[0] == k[0])
 					table[pos].chain=p;
 			}
-			// if wrong position occupied by
-			// now store the existing element in a temporary variable
-			string temp1= table[pos].key;
-			string temp2= table[pos].type;
-			int temp3 = table[pos].chain;
+			else
+			{
+				// if wrong position occupied by existing element
+				// now store the existing element in a temporary variable
+				string temp1= table[pos].key;
+				string temp2= table[pos].type;
+				//int temp3 = table[pos].chain;
+				int tp;
+				// now, find the chain value of the parent of this element
+				// travese the list to find the position of the parent
+				for(int i=0;i<26;i++)
+				{
+					if(table[i].chain == pos)
+					{
+						tp=i;
+						break;
+					}
+				}
+				table[pos].key = k;
+				table[pos].type= t;
+				int tp2=table[pos].chain;
+				table[pos].chain=-1;
+				int p=pos;
+				while(table[p].key != "")
+				{
+					p++;
+				}
+				table[p].key=temp1;
+				table[p].type=temp2;
+				table[tp].chain=p;
+				table[p].chain=tp2;
+
+			}
 
 			// now place the new element at its position
 		}
 	}
+
 	void deleteKey()
 	{
+		string k;
+		cout<<"Enter the key to be deleted"<<endl;
+		//cin>>k;
+		// this code is same as search
+		int pos = searchKey();
+		if(pos == -1)
+			cout<<"Not found"<<endl;
+		else
+		{
+			// first find the chain of the parent
+			int tp;
+			for(int i=0;i<26;i++)
+			{
+				if(table[i].chain == pos)
+				{
+					tp=i;
+					break;
+				}
+			}
+			int tc=table[pos].chain;		// chain of child
 
+			table[pos].key="";
+			table[pos].chain=-1;
+			table[tp].chain=tc;
+			//table[tc].chain=pos;
+		}
 	}
 
-	void searchKey()
+	int searchKey()
 	{
+		int COUNT=0;
 		string k;
 		cout<<"Enter the key to be searched"<<endl;
 		cin>>k;
+		int flag=0;
 		int val=calculateKeyValue(k);
+		if(table[val].key == k)
+			cout<<"Key found at position "<<val<<endl;
+		// case for  - if the position is empty
+		else if(table[val].key == "")
+			cout<<"Key not found "<<endl;
+		else
+		{
+			// if the key is not found
+			// check if chain is not -1
+			int tf=0;  	// take a temporary flag for this special case
+			while(flag == 0 || COUNT<25)
+			{
+				if(table[val].key != "")
+				{
+					if(table[val].key[0] != k[0])
+					{
+						val++;
+						tf=1;
+					}
+				}
+				else if(table[val].key == "")
+					break;
 
+					else if(table[val].chain == -1)
+					{
+						flag=0;
+						val=-1;
+						break;
+					}
+				 if(tf != 1)
+					 val=table[val].chain;
+				if(table[val].key == k)
+				{
+					flag=1;
+					break;
+				}
+				COUNT++;
+			}
+			if(flag == 0)
+				cout<<"Key not found"<<endl;
+			else
+				cout<<"Key found at position "<<val<<endl;
+		}
+		return val;
 	}
 
 	void printKey()
 	{
-		cout<<"Index \tKey \tChain"<<endl;
+		cout<<"Index \tKey \t \tChain"<<endl;
 		for(int i=0;i<26;i++)
-			cout<<i<<" \t "<<table[i].key<<" \t "<<table[i].chain<<endl;
+			cout<<i<<" \t "<<table[i].key<<" \t \t "<<table[i].chain<<endl;
 	}
 
 	int calculateKeyValue(string k)
@@ -212,7 +311,6 @@ public:
 		pos=pos%97;
 		return pos;
 	}
-
 };
 
 
@@ -222,7 +320,7 @@ int main()
 	SymbolTable T;
 	int ch;
 	do{
-	cout<<"1.Insert Key \n2.Print Keys \n9.EXIT"<<endl;
+	cout<<"1.Insert Key(without) \n2.Print Keys \n3.Insert With \n4.Search Key \n5.Delete Key \n9.EXIT"<<endl;
 	cin>>ch;
 	switch(ch)
 	{
@@ -237,6 +335,15 @@ int main()
 	case 3:
 			T.insertWith();
 			break;
+
+	case 4:
+			T.searchKey();
+			break;
+
+	case 5:
+			T.deleteKey();
+			break;
+
 	case 9:
 			break;
 
